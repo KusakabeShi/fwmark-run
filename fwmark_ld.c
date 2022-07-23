3245da3ffe36a4412c51c34e6a26305f850cf40d
kskb <kskb@kskb-vm.home.kskb.dn42>
2022-07-24 01:08:44 +0800

    support hex format

--- fwmark_ld.c
+++ fwmark_ld.c
@@ -4,38 +4,52 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <limits.h>
-#include <sys/socket.h> 
-
-int g_obvio=0;
+#include <sys/socket.h>
+#include <string.h>
 
 #ifndef RTLD_NEXT
 #define RTLD_NEXT ((void *) -1l)
 #endif
-
 #define REAL_LIBC RTLD_NEXT
 
-unsigned int get_so_mark() {
-    const char* so_mark_str = getenv("DEFAULT_SO_MARK");
-    if(!so_mark_str){
+unsigned int get_so_mark()
+{
+    const char *so_mark_str = getenv("DEFAULT_SO_MARK");
+    if (!so_mark_str)
+    {
         return 0;
     }
-    long temp = atol(so_mark_str);
-    if (temp < 0 || temp > INT_MAX){
+
+    long so_mark_long = 0;
+    size_t l = strlen(so_mark_str);
+    if (l > 2 && strncmp(so_mark_str, "0x", 2) == 0)
+    {
+        so_mark_long = strtol(so_mark_str, NULL, 16);
+    }
+    else
+    {
+        so_mark_long = atol(so_mark_str);
+    }
+
+    if (so_mark_long < 0 || so_mark_long > UINT_MAX)
+    {
         return 0;
     }
-    unsigned int so_mark = (unsigned int)temp;
+
+    unsigned int so_mark = (unsigned int) so_mark_long;
     return so_mark;
 };
 
 int socket(int family, int type, int protocol)
 {
-    static int (*real_socket) (int family, int type, int protocol) = NULL;
-    if (!real_socket){
-	    real_socket = (int (*) (int family, int type, int protocol)) dlsym (REAL_LIBC, "socket");
+    static int(*real_socket)(int family, int type, int protocol) = NULL;
+    if (!real_socket)
+    {
+        real_socket = (int(*)(int family, int type, int protocol)) dlsym(REAL_LIBC, "socket");
     }
-    
+
     unsigned int so_mark = get_so_mark();
-    int sockid = real_socket(family,type,protocol);
+    int sockid = real_socket(family, type, protocol);
     setsockopt(sockid, SOL_SOCKET, SO_MARK, &so_mark, sizeof(so_mark));
     return sockid;
 };
